f027d718026bb457e0a24c73e6d0f7ac349d6316
astevenaa <astevenaa@gmail.com>
2022-04-02 23:13:59 -0700

    get and display the cell of terominos

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -4,8 +4,7 @@ import Cell from "./Cell";
 const Stage = ({ stage }) => {
   return (
     <div>
-      <Cell></Cell>
-    </div>
+{stage.map(row => row.map((cell , x) => <Cell key={x} type={cell[0]}/>))}    </div>
   );
 };
 export default Stage;
