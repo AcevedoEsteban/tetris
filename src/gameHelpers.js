23ce15cf43159c904cca459ddbd3a582082f19ee
astevenaa <astevenaa@gmail.com>
2022-04-06 19:58:39 -0700

    turn

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -9,15 +9,15 @@ export const createStage = () =>
 export const checkCollision = (player, stage, { x: moveX, y: moveY }) => {
   for (let y = 0; y < player.tetromino.length; y += 1) {
     for (let x = 0; x < player.tetromino[y].length; x += 1) {
-      //1 check its on a tetromino cell
+      // 1. Check that we're on an actual Tetromino cell
       if (player.tetromino[y][x] !== 0) {
         if (
-          //2 check its if move is inside the stage hieght(y)
-          //should go theough bottom of play area
+          // 2. Check that our move is inside the game areas height (y)
+          // We shouldn't go through the bottom of the play area
           !stage[y + player.pos.y + moveY] ||
-          //3 check that any move is inside the width of game area (x)
+          // 3. Check that our move is inside the game areas width (x)
           !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
-          //4 check that cell move isnt set to clear
+          // 4. Check that the cell wer'e moving to isn't set to clear
           stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !==
             "clear"
         ) {
